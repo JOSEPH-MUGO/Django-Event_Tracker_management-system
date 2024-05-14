0eb282ce22cab024bea004d7c447a8c4cd2a3041
JOSEPH-MUGO <ebt1.03558.21@student.tharaka.ac.ke>
2024-05-14 08:33:36 +0300

    project configuration

---
+++ Eventsystem/EventTracker/account/views.py
@@ -0,0 +1,28 @@
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView,LogoutView
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from .models import *
+
+# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'account/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name ='account/logout.html'
+    next_page = reverse_lazy('login')
+
+class CustomRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'account/reg.html'
+    success_url = reverse_lazy('login')
+
+    def get_queryset(self):
+        return CustomUser.objects.none()
+
+class DashboardView(TemplateView):
+    template_name= 'account/dashboard.html'
+
+
